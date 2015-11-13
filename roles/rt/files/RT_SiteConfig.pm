67234d6fcebdb7723e3bb378a43dceb749fb7298
Brian C. Duggan <brian@dugga.net>
2015-11-13 07:50:51 -0500

    Add RT_SiteConfig.pm

---
+++ roles/rt/files/RT_SiteConfig.pm
@@ -0,0 +1,27 @@
+# Any configuration directives you include  here will override 
+# RT's default configuration file, RT_Config.pm
+#
+# To include a directive here, just copy the equivalent statement
+# from RT_Config.pm and change the value. We've included a single
+# sample value below.
+#
+# This file is actually a perl module, so you can include valid
+# perl code, as well.
+#
+# The converse is also true, if this file isn't valid perl, you're
+# going to run into trouble. To check your SiteConfig file, use
+# this command:
+#
+#   perl -c /path/to/your/etc/RT_SiteConfig.pm
+#
+# You must restart your webserver after making changes to this file.
+
+Set( $rtname, '127.0.0.1');
+Set( $WebPort, 8080);
+
+# You must install Plugins on your own, this is only an example
+# of the correct syntax to use when activating them:
+#     Plugin( "RT::Extension::SLA" );
+#     Plugin( "RT::Authen::ExternalAuth" );
+
+1;
